af41001911bfff32260028eb250a8952da88f498
Kyle Foral <kyleforal@gmail.com>
2023-03-04 00:52:25 +0000

    completed prep-javascript-funtions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+const addTwoNumbersresult = addTwoNumbers(2, 2);
+console.log(addTwoNumbersresult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+const convertHoursToMinutesresult = convertHoursToMinutes(2);
+console.log(convertHoursToMinutesresult);
+
+function getGreeting(name) {
+  return 'Hello ' + name;
+}
+const getGreetingresult = getGreeting('World');
+console.log(getGreetingresult);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+const addAndMultiplyBy5result = addAndMultiplyBy5(10, 5);
+console.log(addAndMultiplyBy5result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+const multiplyAndDivideBy5result = multiplyAndDivideBy5(35, 10);
+console.log(multiplyAndDivideBy5result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+const subtractTwoNumbersresult = subtractTwoNumbers(22, 7);
+console.log(subtractTwoNumbersresult);
+
+function getCircleCircumference(radius) {
+  return (radius + radius) * 3.14;
+}
+const getCircleCircumferenceresult = getCircleCircumference(5);
+console.log(getCircleCircumferenceresult);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+const getFullNameresult = getFullName('Juan', 'Ramirez');
+console.log(getFullNameresult);
+
+function cube(number) {
+  return number * number * number;
+}
+const cuberesult = cube(5);
+console.log(cuberesult);
---
+++ prep-javascript-functions/prep-javascript-functions-quiz-notes.md
@@ -0,0 +1,38 @@
+# prep-javascript-functions-quiz-notes
+
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are the five parts of a function definition?
+The function keyword, an optional name, parameters, code block, optional return
+- How do you call a function?
+By calling its keyword
+- What is the difference between a parameter and an argument?
+a parameter is a place holder while the argument has value.
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
