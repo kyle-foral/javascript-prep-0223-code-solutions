9ed7006ed024559463d493b96f46567e0fb0d7b8
Kyle Foral <kyleforal@gmail.com>
2023-03-10 04:12:33 +0000

    completed prep-object-challenge

---
+++ prep-object-challenge/src/get-keys.js
@@ -0,0 +1,9 @@
+/* exported getKeys */
+
+function getKeys(object) {
+  const array = [];
+  for (const property in object) {
+    array.push(property);
+  }
+  return array;
+}
---
+++ prep-object-challenge/src/get-values.js
@@ -0,0 +1,9 @@
+/* exported getValues */
+
+function getValues(object) {
+  const array = [];
+  for (const property in object) {
+    array.push(object[property]);
+  }
+  return array;
+}
